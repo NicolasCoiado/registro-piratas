66e2d4bd267dcdbd739873d5fd8b3c19167b3a0b
NicolasCoiado <nicolasmcoiado@gmail.com>
2025-07-30 14:29:45 -0300

    chore: Implementa documentação nas rotas de Akuma no mi.

--- src/main/java/br/com/nvnk/RegistroPiratas/Akumanomis/AkumanomiController.java
+++ src/main/java/br/com/nvnk/RegistroPiratas/Akumanomis/AkumanomiController.java
@@ -1,6 +1,12 @@
 package br.com.nvnk.RegistroPiratas.Akumanomis;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -16,8 +22,27 @@ public class AkumanomiController {
     private AkumanomiService service;
 
     @PostMapping
-    @Operation(summary = "Cadastrar Akuma no mi", description = "Cadastra uma Akuma no mi.")
+    @Operation(summary = "Cadastrar akuma no mi", description = "Cadastra uma akuma no mi.")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Akuma no mi cadastrada.",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = AkumanomiDTO.class),
+                            examples = @ExampleObject(
+                                    name = "Exemplo dos dados retornados.",
+                                    summary = "Akuma no Mi",
+                                    externalValue = "/openapi/examples/akumanomi-response.json"
+                            )
+                    )
+            ),
+            @ApiResponse(responseCode = "400", description = "Usuário não cadastrado."),
+            @ApiResponse(responseCode = "404", description = "Akuma no mi não cadastrada."),
+            @ApiResponse(responseCode = "500", description = "Erro inesperado.")
+    })
     public ResponseEntity<?> criarAkumanomi(
+            @Parameter(description = "Dados da akuma no mi.")
             @RequestBody AkumanomiDTO akumanomiEnviada
     ){
         try {
@@ -34,12 +59,50 @@ public class AkumanomiController {
 
 
     @GetMapping
+    @Operation(summary = "Listar akuma no mis", description = "Lista todas as akuma no mis cadastradas.")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Akuma no mis listadas.",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = AkumanomiDTO.class),
+                            examples = @ExampleObject(
+                                    name = "Exemplo dos dados retornados.",
+                                    summary = "Akuma no Mi",
+                                    externalValue = "/openapi/examples/akumanomi-response.json"
+                            )
+                    )
+            ),
+            @ApiResponse(responseCode = "500", description = "Usuário não cadastrado."),
+    })
     public ResponseEntity<List<AkumanomiDTO>> listarAkumanomis(){
         return ResponseEntity.ok(service.listarAkumanomis());
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<?> descreverAkumanomi(@PathVariable Long id) {
+    @Operation(summary = "Descrever uma akuma no mi", description = "Descreve todas as propriedades de uma akuma no mi.")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Akuma no mi descrita.",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = AkumanomiDTO.class),
+                            examples = @ExampleObject(
+                                    name = "Exemplo dos dados retornados.",
+                                    summary = "Akuma no Mi",
+                                    externalValue = "/openapi/examples/akumanomi-response.json"
+                            )
+                    )
+            ),
+            @ApiResponse(responseCode = "404", description = "Akuma no mi não cadastrada."),
+            @ApiResponse(responseCode = "500", description = "Erro inesperado.")
+    })
+    public ResponseEntity<?> descreverAkumanomi(
+            @Parameter(description = "Id da akuma no mi.")
+            @PathVariable Long id
+    ) {
         try {
             return ResponseEntity.ok(service.descreverAkumanomi(id));
         } catch (EntityNotFoundException erro) {
@@ -50,7 +113,30 @@ public class AkumanomiController {
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<?> atualizarAkumanomi(@PathVariable Long id, @RequestBody AkumanomiDTO akumanomi){
+    @Operation(summary = "Atualizar um akuma no mi", description = "Atualiza uma akuma no mi.")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Akuma no mi atualizada.",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = AkumanomiDTO.class),
+                            examples = @ExampleObject(
+                                    name = "Exemplo dos dados retornados.",
+                                    summary = "Akuma no Mi",
+                                    externalValue = "/openapi/examples/akumanomi-response.json"
+                            )
+                    )
+            ),
+            @ApiResponse(responseCode = "400", description = "Usuário não cadastrado."),
+            @ApiResponse(responseCode = "404", description = "Akuma no mi não cadastrada."),
+            @ApiResponse(responseCode = "500", description = "Erro inesperado.")
+    })
+    public ResponseEntity<?> atualizarAkumanomi(
+            @Parameter(description = "Id da akuma no mi.")
+            @PathVariable Long id,
+            @Parameter(description = "Dados da akuma no mi.")
+            @RequestBody AkumanomiDTO akumanomi){
         try {
             return ResponseEntity.ok(service.atualizarAkumanomi(id, akumanomi));
         }catch (EntityNotFoundException erro){
@@ -63,7 +149,30 @@ public class AkumanomiController {
     }
 
     @PatchMapping("/{id}")
-    public ResponseEntity<?> editarAkumanomi(@PathVariable Long id, @RequestBody AkumanomiDTO atualizacoes){
+    @Operation(summary = "Editar um akuma no mi", description = "Edita uma akuma no mi.")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Akuma no mi editada.",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = AkumanomiDTO.class),
+                            examples = @ExampleObject(
+                                    name = "Exemplo dos dados retornados.",
+                                    summary = "Akuma no Mi",
+                                    externalValue = "/openapi/examples/akumanomi-response.json"
+                            )
+                    )
+            ),
+            @ApiResponse(responseCode = "400", description = "Usuário não cadastrado."),
+            @ApiResponse(responseCode = "404", description = "Akuma no mi não cadastrada."),
+            @ApiResponse(responseCode = "500", description = "Erro inesperado.")
+    })
+    public ResponseEntity<?> editarAkumanomi(
+            @Parameter(description = "Id da akuma no mi.")
+            @PathVariable Long id,
+            @Parameter(description = "Dados da akuma no mi.")
+            @RequestBody AkumanomiDTO atualizacoes){
         try {
             return ResponseEntity.ok(service.editarAkumanomi(id, atualizacoes));
         } catch (EntityNotFoundException erro) {
@@ -76,7 +185,16 @@ public class AkumanomiController {
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<String> deletarAkumanomi(@PathVariable Long id){
+    @Operation(summary = "Deletar um akuma no mi", description = "Deleta uma akuma no mi.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Akuma no mi deletada!"),
+            @ApiResponse(responseCode = "404", description = "Akuma no mi não cadastrada."),
+            @ApiResponse(responseCode = "500", description = "Erro inesperado.")
+    })
+    public ResponseEntity<String> deletarAkumanomi(
+            @Parameter(description = "Id da akuma no mi.")
+            @PathVariable Long id
+    ){
         try {
             service.deletarAkumanomi(id);
             return ResponseEntity.ok("Akuma no mi deletada!");
